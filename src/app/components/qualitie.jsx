55640897dc77b79d8d0a9c3540fc71b80aa4af75
Ruslan Gumirov <gumirus29@gmail.com>
2022-09-23 11:07:14 +0500

    correction

--- src/app/components/qualitie.jsx
+++ src/app/components/qualitie.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Qualitie = ({ color, name }) => {
-  const classes = "btn m-2 btn-" + color;
+  const classes = "btn m-1 btn-" + color;
   return <li className={classes}>{name}</li>;
 };
 
