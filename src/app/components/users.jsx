57b15025828215dc5988c829a2c23e6fd68bd3a8
Ruslan Gumirov <gumirus29@gmail.com>
2022-10-13 15:14:02 +0500

    урок-Реализация события сортировки

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
-import User from "./user";
 import api from "../api";
 import PropTypes from "prop-types";
 import GroupList from "./groupList";
 import SearchStatus from "./searchStatus";
+import UserTable from "./usersTable";
 
 const Users = ({ users: allUsers, ...rest }) => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -30,7 +30,7 @@ const Users = ({ users: allUsers, ...rest }) => {
     ? allUsers.filter((user) => user.profession === selectedProf)
     : allUsers;
   const count = filteredUsers.length;
-  const userCrop = paginate(filteredUsers, currentPage, pageSize);
+  const usersGrop = paginate(filteredUsers, currentPage, pageSize);
   const clearFilter = () => {
     setSelectedProf();
   };
@@ -45,38 +45,22 @@ const Users = ({ users: allUsers, ...rest }) => {
               onItemSelect={handleProfessionSelect}
             />
             <button className="btn btn-secondary mt-2" onClick={clearFilter}>
+              {" "}
               Очистить
             </button>
           </div>
         )}
         <div className="d-flex flex-column">
           <SearchStatus length={count} />
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Имя</th>
-                <th scope="col">Качества</th>
-                <th scope="col">Профессия</th>
-                <th scope="col">Встретился, раз</th>
-                <th scope="col">Оценка</th>
-                <th scope="col">Избранное</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {userCrop.map((user) => (
-                <User {...rest} {...user} key={user._id} />
-              ))}
-            </tbody>
-            <div className="d-flex justify-content-center">
-              <Pagination
-                itemsCount={count}
-                pageSize={pageSize}
-                currentPage={currentPage}
-                onPageShange={handlePageChange}
-              />
-            </div>
-          </table>
+          {count > 0 && <UserTable users={usersGrop} {...rest} />}
+          <div className="d-flex justify-content-center">
+            <Pagination
+              itemsCount={count}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageShange={handlePageChange}
+            />
+          </div>
         </div>
       </div>
     ) : (
